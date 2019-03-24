90a5b59d114584df617dc97773274ea42c8676ff
rcrow <r.tykocki.crow@gmail.com>
2019-03-24 23:03:57 +0100

    status messages added, needs proper file output

--- dawkinsWeasel/dawkinsWeasel.R
+++ dawkinsWeasel/dawkinsWeasel.R
@@ -1,17 +1,54 @@
-target = "METHINKS IT IS LIKE A WEASEL";
-species = makeRandomString(1,28);
-cur_score = 0;
-generation = 1;
-
-while ( ! (cur_score == 1)){
+dawkinsWeasel <- function(nGen=0, target, input="", FUN1 = mutate, FUN2 = score) {
   
-  output = generation(species, target, mutate, score, 100);
-  cur_score = output$bestScore;
-  species = output$newBest;
-  generation = generation + 1;
-  print(species);
+  logfile = file("log", open = "wt");
+  target_length = nchar(target);
+  
+  if ( is.null(input) || !(nchar(input) == target_length)) {
+    species = makeRandomString(1,target_length);
+    message("Input string not specified or of incompatible length...input replaced with random string");
+    writeLines("Input string not specified or of incompatible length...input replaced with random string \n", logfile);
+  }
+  else {
+    species = input;
+  }
+
+  cur_score = score(species, target);
+  iter = 1;
+  message("Generation:\t", iter, "\t Current score:\t", cur_score, "\t Species:\t", species);
+  writeLines(c("Generation:", iter, "Current score:", cur_score, "Species:", species,"\n"), logfile);
+  
+  if (nGen == 0) {
+    
+    while ( ! (cur_score == 1)){
+      
+      output = generation(species, target, mutate, score, 100);
+      cur_score = output$bestScore;
+      species = output$newBest;
+      iter = iter + 1; 
+      message("Generation:\t", iter, "\t Current score:\t", cur_score, "\t Species:\t", species);
+      #cat(c("Generation:\t", iter, "\t Current score:\t", cur_score, "\t Species:\t", species, "\n"), logfile);
+      
+    }
+    
+  }
+  else {
+    
+    for (i in 1:(nGen-1)) {
+      
+      output = generation(species, target, mutate, score, 100);
+      cur_score = output$bestScore;
+      species = output$newBest;
+      message("Generation:\t", i+1, "\t Current score:\t", cur_score, "\t Species:\t", species);
+      #cat(c("Generation:\t", i+1, "\t Current score:\t", cur_score, "\t Species:\t", species), "log");
+      
+    }
+    
+  }
+  
+  close(logfile)
   
 }
+
 
 
 
