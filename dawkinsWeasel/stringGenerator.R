7f7afe432e9578e4f32435e9b6ae82b4f25a373d
robertcrow <r.crow@quickersim.com>
2019-04-03 15:02:28 +0200

    for loop removed from string generator

--- dawkinsWeasel/stringGenerator.R
+++ dawkinsWeasel/stringGenerator.R
@@ -1,11 +1,16 @@
-makeRandomString <- function(n_strings, char_length)
+nRandomStrings <- function(n_strings, char_length)
 {
-  randomString <- c(1:n_strings)                  # initialize vector
-  for (i in 1:n_strings)
-  {
-    randomString[i] <- paste(sample(c(LETTERS),
-                      char_length, replace=TRUE),
-                             collapse="")
+  randomString <- c(1:n_strings) # initialize vector
+  genString <- function(char_len) paste(sample(c(LETTERS),char_len, replace=TRUE),collapse="")
+
+  if (n_strings == 1) {
+    randomString <- genString(char_length)
+
+  } else if (n_strings > 1) {
+    dummy = rep(28, n_strings)
+    randomString = lapply(dummy, genString)
+
   }
+
   return(randomString)
 }
