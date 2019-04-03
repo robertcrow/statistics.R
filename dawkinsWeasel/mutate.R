d67ceefdd639ad5181cec371bd535ec6657558c5
robertcrow <r.crow@quickersim.com>
2019-04-03 19:25:48 +0200

    weasel working without for loops

--- dawkinsWeasel/mutate.R
+++ dawkinsWeasel/mutate.R
@@ -1,30 +1,36 @@
-mutate <- function(arg1){
-  
-  n_strings = length(arg1);
-  output = vector(mode="character", length=n_strings);
-  index = 0;
-  
-  for (i in 1:n_strings) {
-    
-    current_char = arg1[i];
+mutate <- function(input_strings) {
 
-    if (length(current_char) == 1)
-      current_char = strsplit(current_char, split="")
-      current_char = unlist(current_char);
-    nchar = length(current_char);
-    
-    criteria = runif(nchar, min=0, max=100);
-    chars_for_mutation = criteria <= 5.0;
-    n_chars_for_mutation = sum(chars_for_mutation);
-      
-    current_char[chars_for_mutation] = sample(c(LETTERS, " "), n_chars_for_mutation, replace=TRUE);
-      
-    output_char = paste((current_char),collapse="")
-    output[i] = output_char;
-    
+
+  n_strings = length(input_strings)
+
+  output = vector(mode = "character", length = n_strings)
+
+  stringMutation <- function(input_strings) {
+
+    current_char = input_strings
+
+
+    if (length(current_char) == 1) {
+      current_char = strsplit(current_char, split = "")
+      current_char = unlist(current_char)
+    }
+
+    nchar = length(current_char)
+    criteria = runif(nchar, min = 0, max = 100)
+    chars_for_mutation = criteria <= 5.0
+    n_chars_for_mutation = sum(chars_for_mutation)
+
+    current_char[chars_for_mutation] = sample(c(LETTERS, " "), n_chars_for_mutation, replace =
+                                                TRUE)
+
+    output_char = paste((current_char), collapse = "")
+
   }
-  
-  return(output);
-  
-}
 
+  output = lapply(input_strings, stringMutation)
+  output = unlist(output)
+
+  return(output)
+
+
+}
--- dawkinsWeasel/score.R
+++ dawkinsWeasel/score.R
@@ -1,30 +1,29 @@
 score <- function(a, b){
-  
+
   #stopifnot(length(a) == length(b))
-  stopifnot(is.character(a));
   stopifnot(is.character(b));
   n_strings = length(a);
+
   coverage = rep(0, n_strings);
-  
-  
-  for (i in 1:n_strings){
-    
-    a_tmp = a[i];
-    
-    tmp1 = strsplit(a_tmp, split="");
-    tmp2 = strsplit(b, split="");
-    
-    tmp1 = unlist(tmp1);
-    tmp2 = unlist(tmp2);
-    
-    nmatches<-sum(tmp1==tmp2);
-    nchar = length(tmp1);
-    
-    coverage[i] = nmatches / nchar;
-    
+
+  compareStrings <- function(arg1, arg2) {
+
+    tmp1 = strsplit(arg1, split="")
+    tmp2 = strsplit(arg2, split="")
+
+    tmp1 = unlist(tmp1)
+    tmp2 = unlist(tmp2)
+
+    nchar = length(tmp1)
+
+    return (sum(tmp1 == tmp2) / nchar)
   }
 
+  coverage = lapply(a, FUN=compareStrings, arg2=b)
+  coverage = unlist(coverage)
+
+
   return(coverage)
-  
+
 }
 
