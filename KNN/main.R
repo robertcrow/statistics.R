25162587003424f0f0bfeb22c404569100ab9b6a
rcrow <r.tykocki.crow@gmail.com>
2019-04-25 09:56:03 +0200

    working KNN

--- KNN/main.R
+++ KNN/main.R
@@ -13,11 +13,13 @@ B = B[, c(1:ds_dim[2]- 1)]
 
 euclidian_norm <- function(vecs, ref_vec) {sqrt(sum((vecs - ref_vec)^2 ))}
 
-dists <- function(x, ref_row) {
+dists <- function(vecs, ref_vec) {
   
-  ref_row = 1    
-  ref_vec = x[ref_row, ]
-  vecs = x[-ref_row, ]
+  # sprobowac jako MACIERZ !!!
+  
+  #ref_vec = x[ref_row, ]
+  #vecs = x[-ref_row, ]
+  #vecs = x[rows,]
   
   dists = apply(vecs, 1, FUN=euclidian_norm, ref_vec)
   return (dists)
@@ -41,9 +43,11 @@ kNNsingle <- function(a, B, Y, k){
 
 kNN <- function(trainX, trainY, testX, k){
   
-  mine_or_rock = apply(X=testX, MARGIN=1, FUN=kNNsingle, trainX=trainX, trainY=trainY, k=k)
+  mine_or_rock = apply(X=testX, MARGIN=1, FUN=kNNsingle, B=trainX, Y=trainY, k=k)
   return (mine_or_rock)
   
 }
 
+predictions = kNN(B[seq(1,208,2),], sonar_class[seq(1,208,2)], B[seq(2,208,2),], 5)
+table(predictions, sonar_class[seq(2,208,2)])
 
